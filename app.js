45214664de19123b392fb4f48728519a6920fdf4
test user <student1@cec.edu.ph>
2016-07-07 18:55:24 +0800

    BasicStructures

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+var age = 15;
+var pi = 3.1416;
+var date = new Date();
+var computeArea = function(l,w){
+    return 1 * w;
+}
+
+var name = "Lea Dapiton";
+console.log('Age:' + age);
+console.log('PI' + pi.toFixed(2));
+console.log(date);
+console.log(name);
+console.log(computeArea(10,60));
+
+var x = 1;
+var y = null;
+var waladihauyy = undefined;
+
+if (x)
+console.log('x is true');
+
+if (!y)
+console.log('y is false');
+
+if (!waladihauyy)
+console.log('waladihauyy is undefined');
+
+
+//===========JSON Javascript Object Notation;
+ var jsonObject = {
+     name: 'Lea',
+     lastname: 'Dapiton',
+     address: 'Gimpo'
+ };
+ 
+ console.log(jsonObject.name);
+ console.log(jsonObject.lastname);
+ console.log(jsonObject.address);
+ console.log(jsonObject);
+ 
